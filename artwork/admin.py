ec590b03ad4f94265ceb6cf7769c86e7cc7aad1b
MasonCodyCox <masonfraser3@googlemail.com>
2021-07-03 19:57:15 +0000

    Removed json files and added data via the allauth admin panel. also customises admin panel

--- artwork/admin.py
+++ artwork/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Artwork, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Artwork, ArtworkAdmin)
+admin.site.register(Category, CategoryAdmin)
