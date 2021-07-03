e11451a7e1e09dbcc71b0c3dc6e1beb2258c6f47
MasonCodyCox <masonfraser3@googlemail.com>
2021-07-03 21:18:54 +0000

    added artwork views and template

--- artwork/views.py
+++ artwork/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Artwork
 
 # Create your views here.
+
+def all_artwork(request):
+    """ A view to show all items """
+
+    artwork = Artwork.objects.all()
+
+    context = {
+        'artwork': artwork,
+    }
+
+    return render(request, 'artwork/artwork.html', context)
