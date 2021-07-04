ce39b01eddd9985db145c39da4112b2bc04679d9
MasonCodyCox <masonfraser3@googlemail.com>
2021-07-04 10:52:05 +0000

    starts to connect site to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn neve_artwork.wsgi:application
--- artwork/urls.py
+++ artwork/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',  views.all_artwork, name='artwork')
+    path('',  views.all_artwork, name='artwork'),
+    path('<artwork_id>',  views.artwork_detail, name='artwork_detail')
 ]
